962ed20a8bf440bb455de62305a6156b84f55c6d
JohnDoe1112 <johndoe11@posteo.de>
2022-08-30 21:03:49 +0200

    Explore SQLite database integration with diesel
    The code was inspired by the diesel SQLite example to get used
    to the diesel interface and command line.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
-use std::error::Error;
+use std::{error::Error, env, io::{stdin, Read}};
 
+use chrono::NaiveDateTime;
 use clap::{Parser, Subcommand};
+use diesel::{Queryable, SqliteConnection, Connection, prelude::*};
+use dotenvy::dotenv;
 use inquire::{DateSelect, Confirm, CustomType, MultiSelect, Select};
+mod schema;
+use schema::posts;
 
+#[derive(Queryable)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+}
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut SqliteConnection, title: &str, body: &str) -> usize {
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Error saving new post")
+}
 
 #[derive(Parser)]
 struct Cli {
@@ -65,6 +101,49 @@ fn do_inquire_stuff() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
+
+    let connection = &mut establish_connection();
+    println!("What would you like your title to be?");
+    let mut titlestr = String::new();
+    stdin().read_line(&mut titlestr).unwrap();
+    let titlestr = &titlestr[..(titlestr.len() - 1)]; // Drop the newline character
+    println!(
+        "\nOk! Let's write {} (Press {} when finished)\n",
+        titlestr, "CTRL+D"
+    );
+    let mut mybody = String::new();
+    stdin().read_to_string(&mut mybody).unwrap();
+
+    let _ = create_post(connection, titlestr, &mybody);
+    println!("\nSaved draft {}", titlestr);
+
+    use self::schema::posts::dsl::*;
+
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
+    let results = posts
+        .filter(published.eq(false))
+        .limit(5)
+        .load::<Post>(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} unpublished posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
+    return;
     let cli = Cli::parse();
     match cli.action {
         Action::Start => {
